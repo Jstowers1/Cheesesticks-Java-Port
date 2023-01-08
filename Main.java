4cf63513f04674d0aaebd934ee40e0a00d8e419f
Johnathan Stowers <Lunkman728@hotmail.com>
2023-01-08 13:57:43 -0600

    Produced deckToString method for easy framework

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.*;
+
 class Main {
   @Override
   public String toString() {
@@ -10,6 +11,7 @@ class Main {
   //Master Deck, do not touch!
   public static class Global {
     public static Object[] deck = new Object[] {"2","3","4","5","6","7","8","9","10","J","Q","K","2","3","4","5","6","7","8","9","10","J","Q","K","2","3","4","5","6","7","8","9","10","J","Q","K","2","3","4","5","6","7","8","9","10","J","Q","K","A","A","A","A"};
+    public static String[] oneSet = new String[]{"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
   }
   
   //Add/Subtract to a list
@@ -66,20 +68,55 @@ class Main {
   }
 
   //Produce the arrays that will be used for the bot players
-  public static Object[][] botDeck(int botCount){
+  public static Object[][] playerDeck(int botCount){
     Object[][] botCards = new Object[botCount][1];
     for(int i = 0; i<botCount;i++){
-      Object[][] botCard = new Object[][]{{i+2, cardDivide()}};
+      Object[][] botCard = new Object[][]{{i+1, cardDivide()}};
       botCards[i] = botCard;
     }
     return botCards;
   }
   
+  //Splits the Chew-Dee array into a string, fit for the player and use
+  public static String deckToString(Object[][] playerCards, int playerNum){
+    String playerDeck = Arrays.deepToString(playerCards);
+    playerDeck = playerDeck.substring(1, playerDeck.length()-1);
+    System.out.println(playerDeck);
+    for(int i = 0; i < (playerNum)*3; i++){
+      playerDeck = playerDeck.substring(playerDeck.indexOf("[")+1,playerDeck.length());
+    }
+    playerDeck = playerDeck.substring(0,playerDeck.indexOf("]"));
+    System.out.println(playerDeck);
+    return playerDeck;
+  }
+
+  //Check to see if there is 4 of a kind in the deck
+  public static boolean cheeseCheck(Object[][] playerDe, int playerNum){
+    int count = 0;
+    playerNum--;
+    Object[] x23 = new Object[]{"4","4","4","4","5","6","7"};
+    Object[][] playerDec = new Object[][]{{1, x23}};
+    String playerDeck = new String(deckToString(playerDec, playerNum));
+    for(int i = 0; i < playerDeck.length();i++){
+      count = 0;
+      for(int i2 = 0; i2 < Global.oneSet.length;i2++){
+        if((playerDeck.equals(Global.oneSet[i2]))){
+          count++;
+          System.out.println(count);
+        }
+        if(count%4 == 0 && count>0){
+          System.out.println(count);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     System.out.println("Welcome to Cheesesticks!");
     Global.deck = shufle();
-    Player one = new Player(cardDivide(), 1, false);
     //Get the proper number of other bots
     int playerCount = 1;
     System.out.println("How many bots do you want to play with? Answer 2 - 7.");
@@ -88,17 +125,25 @@ class Main {
       System.out.println("Enter a proper value. 2-7 bots?");
       playerCount = scan.nextInt();
     }
-    int botCount = playerCount-1;
-    Object[][] botCards = new Object[botCount][1];
+    Object[][] playCards = new Object[playerCount][1];
     //Use two-dimensional arrays to hold information for players
     //D1 - Player Number
     //D2 - Deck for each player 
     //Maybe turn this into an array of objects? Would make sense in the end 
     //11/6 signoff
-    botCards = botDeck(botCount);
-    System.out.println(one);
+    playCards = playerDeck(playerCount);
+
+    String oneCards = new String(deckToString(playCards, 1));
+    System.out.printf("Here are your cards: %s\n", oneCards);
+   
+    /*
+    boolean cheesestick = cheeseCheck(playCards, 1);
+    if (cheesestick){
+      System.out.println("CHEESE DETECTED");
+    }else{
+      System.out.println("no cheese :(");
+    }
+    */
     scan.close();
-    //Object[][] x = new Object[][]{{"HELLo", "help"}};
-    System.out.println(Arrays.deepToString(botCards));
   }
 }
